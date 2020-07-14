c5ae473147753e1e9f8ca85a381b34749adb2d75
flippedbit <straughan.michael@gmail.com>
2020-07-14 02:18:23 -0400

    switching to XGBoost model. added Pipeline. changed up model parameter optimization. changed data preprocessing from dropping empty columns to using SimpleImputer and OneHotEncoder

--- kaggle/home-data-for-ml-course/model.py
+++ kaggle/home-data-for-ml-course/model.py
@@ -1,74 +1,79 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from xgboost import XGBRegressor
 
-def find_max_leaf(train_X, val_X, train_y, val_y, max_leaf = 2, previous_rmse = (0,0), prev_max_leaf = 0):
-    rf_model = RandomForestRegressor(max_leaf_nodes = max_leaf, criterion = "mse", random_state=1)
-    rf_model.fit(train_X, train_y)
-    pred = rf_model.predict(val_X)
-    rmse = mean_squared_error(val_y, pred)
-
-    last_rmse, prev_rmse = previous_rmse
-    if prev_max_leaf == 0:
-        return find_max_leaf(train_X, val_X, train_y, val_y, max_leaf = max_leaf * 2, previous_rmse = (rmse, rmse), prev_max_leaf = max_leaf)
-
-    #print("%d: %d (last: %d, prev: %d)" % (max_leaf, rmse, last_rmse, prev_rmse))
-    if rmse > last_rmse:
-        if rmse < prev_rmse:
-            return find_max_leaf(train_X, val_X, train_y, val_y, max_leaf = int(prev_max_leaf * 0.5), previous_rmse=(rmse, prev_rmse), prev_max_leaf = max_leaf)
-    else:
-        return find_max_leaf(train_X, val_X, train_y, val_y, max_leaf = max_leaf * 2, previous_rmse = (rmse, last_rmse), prev_max_leaf = max_leaf)
-    return (prev_max_leaf, last_rmse)
-    
-def find_estimators(train_X, val_X, train_y, val_y, max_leaf, estimators = 1, previous_rmse = (0,0), previous_estimators = 0):
-    rf_model = RandomForestRegressor(n_estimators = estimators, max_leaf_nodes = max_leaf, criterion = "mse", random_state=1)
-    rf_model.fit(train_X, train_y)
-    pred = rf_model.predict(val_X)
-    rmse = mean_squared_error(val_y, pred)
-
-    last_rmse, prev_rmse = previous_rmse
-    if previous_estimators == 0:
-        return find_estimators(train_X, val_X, train_y, val_y, max_leaf, estimators = (estimators * 2), previous_rmse = (rmse, rmse), previous_estimators = estimators)
-    
-    #print("%d: %d (last: %d, prev: %d" % (estimators, rmse, last_rmse, prev_rmse))
-
-    if rmse > last_rmse:
-        if rmse < prev_rmse:
-            return find_estimators(train_X, val_X, train_y, val_y, max_leaf, estimators = (estimators * 0.5), previous_rmse = (rmse, rmse), previous_estimators = estimators)
-    else:
-        return find_estimators(train_X, val_X, train_y, val_y, max_leaf, estimators = (estimators * 2), previous_rmse = (rmse, rmse), previous_estimators = estimators)
-    return (previous_estimators, last_rmse)
+def get_score(X_train, y_train, n_estimators = 10, learning_rate = 0.1):
+    from sklearn.model_selection import cross_val_score
+    my_pipeline = Pipeline(steps=[
+                            ('preprocessor', preprocessor),
+                            ('model', XGBRegressor(random_state=0, n_estimators=n_estimators, learning_rate=learning_rate)),
+                        ])
+    scores = -1 * (cross_val_score(my_pipeline, X_train, y_train, cv=3, scoring='neg_mean_absolute_error'))
+    return scores.mean()
 
 print("Opening data...")
 file_path = "train.csv"
 train_data = pd.read_csv(file_path)
+
+print("Pre-process data")
+train_data.dropna(axis=0, subset=['SalePrice'], inplace=True)
 y = train_data.SalePrice
-features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
-X = train_data[features]
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+train_data.drop(['SalePrice'], axis=1, inplace=True)
 
-print("Finding optimal max_node...")
-max_nodes, rmse = find_max_leaf(train_X, val_X, train_y, val_y)
-print("%d max_nodes yields %d mean_squared_error" % (max_nodes, rmse))
-print("Finding optimal estimators...")
-max_estimators, rmse = find_estimators(train_X, val_X, train_y, val_y, max_nodes)
-print("%d estimators yields %d mean_squared_error" % (max_estimators, rmse))
+train_X_full, val_X_full, train_y, val_y = train_test_split(train_data, y, random_state=0)
 
-print("Removing columns with empty data...")
-missing_columns = [col for col in X.columns if X[col].isnull().any()]
-final_X = X.drop(missing_columns, axis = 1)
+low_cardinality_cols = [col for col in train_X_full.columns if train_X_full[col].nunique() < 10 and train_X_full[col].dtype == "object"]
+num_cols = [col for col in train_X_full.columns if train_X_full[col].dtype in ['int64', 'float64']]
+cols = low_cardinality_cols + num_cols
 
-print("Training data with %d nodes and %d estimators" % (max_nodes, max_estimators))
-rf_model = RandomForestRegressor(n_estimators = max_estimators, max_leaf_nodes = max_nodes, criterion = "mse", random_state = 1)
-rf_model.fit(final_X, y)
+train_X = train_X_full[cols].copy()
+val_X = val_X_full[cols].copy()
+
+numerical_transofrmer = SimpleImputer(strategy='constant')
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant')),
+    ('onehost', OneHotEncoder(handle_unknown='ignore'))
+])
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transofrmer, num_cols),
+        ('cat', categorical_transformer, low_cardinality_cols)
+    ]
+)
+
+print("Optimize model parameters")
+estimators_results = {}
+for num in range(50, 1000, 50):
+    estimators_results[num] = get_score(X_train=train_X, y_train=train_y, n_estimators=num)
+n_estimators=min(estimators_results, key = lambda i: estimators_results[i])
+print(" - using %d n_estimators in model (MAE: %.5f)" % (n_estimators, estimators_results[n_estimators]))
+
+learning_results = {}
+from numpy import arange
+for num in arange(0.01, 0.2, 0.01):
+    learning_results[num] = get_score(X_train = train_X, y_train = train_y, n_estimators = n_estimators, learning_rate = num)
+learning_rate = min(learning_results, key = lambda i: learning_results[i])
+print(" - using %.2f learning_rate in model (MAE: %.5f)" % (learning_rate, learning_results[learning_rate]))
+
+model = XGBRegressor(random_state = 0, n_estimators = n_estimators, learning_rate = learning_rate)
+pipeline = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', model)
+])
+
+pipeline.fit(train_X, train_y)
 
 print("Predicting values...")
 file_path = "test.csv"
 test_data = pd.read_csv(file_path)
-X = test_data[final_X.columns]
-df = pd.DataFrame(data={"Id": test_data['Id'], "SalePrice": rf_model.predict(X)}, index=None)
+X = test_data[cols].copy()
+df = pd.DataFrame(data={"Id": test_data['Id'], "SalePrice": pipeline.predict(X)}, index=None)
 print("Values:")
 print(df.head())
 print("Saving...")
-# df.to_csv("prediction.csv", index=False)
+df.to_csv("gboosted_prediction.csv", index=False)
